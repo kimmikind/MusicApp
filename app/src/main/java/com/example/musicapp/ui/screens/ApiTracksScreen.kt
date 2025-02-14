46d8ab3ec17cf4cc4f5326d0db3c6112bd68a27c
Мария Самойлова <kimmikind@yandex.ru>
2025-02-14 13:32:24 +0300

    bottom navigation

---
+++ app/src/main/java/com/example/musicapp/ui/screens/ApiTracksScreen.kt
@@ -0,0 +1,17 @@
+package com.example.musicapp.ui.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun ApiTracksScreen() {
+    Text(
+        modifier = Modifier.fillMaxSize().wrapContentSize(),
+        text = "Api",
+        textAlign = TextAlign.Center
+    )
+}
